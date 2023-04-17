e11bd4a34fc527280c526e14b9673cd12e27a9bd
Tait Hoyem <tait@tait.tech>
2023-04-17 15:25:56 -0600

    Update version

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ use sqlx::{Pool, Postgres};
 use std::net::SocketAddr;
 use std::sync::Arc;
 
-const VERSION: &str = "0.3.2";
+const VERSION: &str = "0.3.3";
 
 #[derive(Template)]
 #[template(path = "hello.html")]
