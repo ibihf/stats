3109d92adea4585842e766c3b17ed5eac9912c0a
Tait Hoyem <tait@tait.tech>
2023-04-19 16:32:53 -0600

    Add nullable filter

--- src/filters.rs
+++ src/filters.rs
@@ -3,3 +3,9 @@ pub fn seconds_as_time(secs: &i32) -> ::askama::Result<String> {
     let seconds = secs % 60;
     Ok(format!("{}:{}", minutes, seconds))
 }
+pub fn nullable<T: std::fmt::Display>(ot: &Option<T>) -> ::askama::Result<String> {
+  match ot {
+    Some(t) => Ok(format!("{}", t)),
+    None => Ok("NULL".to_string())
+  }
+}
